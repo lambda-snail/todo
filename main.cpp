7206363a311045e404bfa27b0359b4f19b49930d
lambda-snail <>
2025-06-15 12:46:37 +0200

    merge todo application into auth application

--- main.cpp
+++ main.cpp
@@ -22,129 +22,12 @@
 #include <Wt/WStackedWidget.h>
 #include <Wt/WTemplate.h>
 
-class TodoApplication final : public Wt::WApplication
-{
-  public:
-    explicit TodoApplication(const Wt::WEnvironment& env);
-
-  private:
-    Wt::WStackedWidget* m_widget_stack;
-
-    Wt::WPushButton* m_button;
-
-    void showTodoList();
-
-    void handleInternalPath(std::string const& path);
-};
-
-/*
- * The env argument contains information about the new session, and
- * the initial request. It must be passed to the WApplication
- * constructor so it is typically also an argument for your custom
- * application constructor.
- */
-TodoApplication::TodoApplication(const Wt::WEnvironment& env) : WApplication(env)
-{
-    setTitle("Hello world"); // application title
-
-    auto t = std::make_unique<Wt::WTemplate>(Wt::WString::tr("main-layout"));
-
-    // m_button = t->bindWidget("greeting-button", std::make_unique<Wt::WPushButton>("Greet!"));
-    //  m_button->clicked().connect(this, &TodoApplication::showTodoList);
-
-    t->doJavaScript("window.toggleSidebar = function() { const sidebar = "
-                    "document.querySelector('.sidebar'); sidebar.classList.toggle('collapsed'); }");
-
-    m_widget_stack = t->bindWidget("main-content", std::make_unique<Wt::WStackedWidget>());
-
-    // m_button = m_widget_stack->addNew<Wt::WPushButton>("Greet!");
-    // m_button->clicked().connect(this, &TodoApplication::showTodoList);
-
-    // auto* item = m_widget_stack->addNew<todo_item>("My TODO Item");
-    //  auto* item = m_widget_stack->addWidget(std::move(std::make_unique<todo_item_view>("My TODO
-    //  Item", 1)));
-    //
-    //  item->register_on_checked([]() { std::cout << "checked" << std::endl; });
-    //   item->register_on_unchecked([]() { std::cout << "UNchecked" << std::endl; });
-
-    auto* item =
-        m_widget_stack->addWidget(std::move(std::make_unique<LambdaSnail::todo::todo_page>()));
-
-    m_widget_stack->setCurrentWidget(item);
-
-    // WApplication::instance()->internalPathChanged().connect(
-    //     this, &TodoApplication::handleInternalPath);
-
-    root()->addWidget(std::move(t));
-
-    // root()->addWidget(std::make_unique<Wt::WText>("Your name, please ? ")); //
-    // show some text
-    //
-    // nameEdit_ = root()->addWidget(std::make_unique<Wt::WLineEdit>()); // allow
-    // text input nameEdit_->setFocus();                              // give
-    // focus
-    //
-    // auto button = root()->addWidget(std::make_unique<Wt::WPushButton>("Greet
-    // me."));
-    //                                                     // create a button
-    // button->setMargin(5, Wt::Side::Left);                   // add 5 pixels
-    // margin
-    //
-    // root()->addWidget(std::make_unique<Wt::WBreak>());    // insert a line
-    // break greeting_ = root()->addWidget(std::make_unique<Wt::WText>()); //
-    // empty text
-    //
-    // /*
-    //  * Connect signals with slots
-    //  *
-    //  * - simple Wt-way: specify object and method
-    //  */
-    // button->clicked().connect(this, &HelloApplication::greet);
-    //
-    // /*
-    //  * - using an arbitrary function object, e.g. useful to bind
-    //  *   values with std::bind() to the resulting method call
-    //  */
-    // nameEdit_->enterPressed().connect(std::bind(&HelloApplication::greet,
-    // this));
-    //
-    // /*
-    //  * - using a lambda:
-    //  */
-    // button->clicked().connect([this]() {
-    //     std::cerr << "Hello there, " << nameEdit_->text() << std::endl;
-    // });
-}
-
-void TodoApplication::handleInternalPath(std::string const& path)
-{
-    // if (session_.login().loggedIn()) {
-    if (path == "/list") {
-        // m_widget_stack->setCurrentWidget();
-    }
-
-    else
-        WApplication::instance()->setInternalPath("/play", true);
-    //}
-}
-
 // TODO: Merge into todo application
-class AuthApplication : public Wt::WApplication
+class todo_application final : public Wt::WApplication
 {
   public:
-    explicit AuthApplication(const Wt::WEnvironment& env,
-                             Wt::Auth::AuthService& auth_service,
-                             Wt::Auth::PasswordService& password_service,
-                             std::vector<std::unique_ptr<Wt::Auth::OAuthService>>& oauth_services)
-        : Wt::WApplication(env),
-          m_session(appRoot() + "auth.db", auth_service, password_service, oauth_services)
+    void setup_auth_page()
     {
-        m_session.login().changed().connect(this, &AuthApplication::authEvent);
-        WApplication::instance()->internalPathChanged().connect(
-            this, &AuthApplication::handleInternalPath);
-
-        // root()->addStyleClass("container");
-
         auto auth_page = std::make_unique<LambdaSnail::todo::pages::authentication_page>(m_session);
 
         auth_page->model()->addPasswordAuth(&m_session.password_auth());
@@ -155,7 +38,27 @@ class AuthApplication : public Wt::WApplication
         root()->addWidget(std::move(auth_page));
     }
 
-    void setUpAuthenticatedContent()
+    explicit todo_application(const Wt::WEnvironment& env,
+                             Wt::Auth::AuthService& auth_service,
+                             Wt::Auth::PasswordService& password_service,
+                             std::vector<std::unique_ptr<Wt::Auth::OAuthService>>& oauth_services)
+        : Wt::WApplication(env),
+          m_session(appRoot() + "auth.db", auth_service, password_service, oauth_services)
+    {
+        m_session.login().changed().connect(this, &todo_application::authEvent);
+        WApplication::instance()->internalPathChanged().connect(
+            this, &todo_application::handle_internal_path);
+
+        if (m_session.login().loggedIn())
+        {
+            setInternalPath("/list", true);
+        }
+        else {
+            setup_auth_page();
+        }
+    }
+
+    void setup_authenticated_content()
     {
         auto t = std::make_unique<Wt::WTemplate>(Wt::WString::tr("main-layout"));
 
@@ -165,49 +68,51 @@ class AuthApplication : public Wt::WApplication
 
         m_widget_stack = t->bindWidget("main-content", std::make_unique<Wt::WStackedWidget>());
 
-        auto* item =
+        m_todo_page =
             m_widget_stack->addWidget(std::move(std::make_unique<LambdaSnail::todo::todo_page>()));
 
-        m_widget_stack->setCurrentWidget(item);
-
-        root()->clear();
         root()->addWidget(std::move(t));
     }
 
     void authEvent()
     {
         if (m_session.login().loggedIn()) {
-            setInternalPath("/list", true);
             const Wt::Auth::User& u = m_session.login().user();
             log("notice") << "User " << u.id() << " (" << u.identity(Wt::Auth::Identity::LoginName)
                           << ")"
                           << " logged in.";
 
-            //setUpAuthenticatedContent();
-        } else
+            root()->clear();
+            setup_authenticated_content();
+            setInternalPath("/list", true);
+        }
+        else
+        {
+            root()->clear();
+            setup_auth_page();
             log("notice") << "User logged out.";
+        }
     }
 
-    void handleInternalPath(std::string const& path)
+    void handle_internal_path(std::string const& path)
     {
-        log("path") << path;
-
-        setUpAuthenticatedContent();
-
-        // // if (session_.login().loggedIn()) {
-        // if (path == "/list") {
-        //     // m_widget_stack->setCurrentWidget();
-        // }
-        //
-        // else
-        //     WApplication::instance()->setInternalPath("/play", true);
-        // //}
+        if (m_session.login().loggedIn()) {
+            if (path == "/list") {
+                m_widget_stack->setCurrentWidget(m_todo_page);
+            }
+            else
+            {
+                //WApplication::instance()->setInternalPath("/play", true);
+            }
+        }
     }
 
   private:
     LambdaSnail::todo::application::Session m_session;
 
-    Wt::WStackedWidget* m_widget_stack;
+    Wt::WStackedWidget* m_widget_stack{};
+
+    LambdaSnail::todo::todo_page* m_todo_page{};
 };
 
 // TODO: Find some better place to put this
@@ -251,14 +156,14 @@ int main(int argc, char** argv)
     Wt::Auth::PasswordService password_service(auth_service);
     std::vector<std::unique_ptr<Wt::Auth::OAuthService>> oauth_services;
 
-    configure_auth(auth_service, password_service, oauth_services);
-
     try {
         Wt::WServer server{argc, argv, WTHTTP_CONFIGURATION};
 
+        configure_auth(auth_service, password_service, oauth_services);
+
         server.addEntryPoint(Wt::EntryPointType::Application, [&](const Wt::WEnvironment& env) {
             // auto app = std::make_unique<TodoApplication>(env);
-            auto app = std::make_unique<AuthApplication>(
+            auto app = std::make_unique<todo_application>(
                 env, auth_service, password_service, oauth_services);
 
             app->setTheme(std::make_shared<Wt::WBootstrap5Theme>());
